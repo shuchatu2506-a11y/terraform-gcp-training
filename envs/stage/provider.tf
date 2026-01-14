940503a0e050afdf65c75c88a0fc8c0bca913ab6
Shubham Chaturvedi <shuchatu2506@gmail.com>
2026-01-14 09:27:47 +0530

    Add terraform CI/CD pipeline

--- envs/stage/provider.tf
+++ envs/stage/provider.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "gcs" {
-    bucket  = "gcp-devops-terraform-state-481206"
-    prefix  = "terraform/dev"
-  }
-}
-
 provider "google" {
   project = var.project_id
   region  = var.region
